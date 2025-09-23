441781d05b85e14c88a94a47fbc30e8c6e8b2e7a
unknown <chemmasterynahida@gmail.com>
2025-09-23 21:25:58 +0900

    first publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://chemmasterynahida.github.io/",
-	base: "/chemmasterynahida.github.io/",
+	base: "/",
 	trailingSlash: "always",
 	output: 'static',
 	integrations: [
