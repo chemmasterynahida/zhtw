b3fe687a08b3d0228554960139ff88e51c4e1040
unknown <chemmasterynahida@gmail.com>
2025-10-25 18:45:15 +0900

    atomic model

--- src/components/StudyPanel.svelte
+++ src/components/StudyPanel.svelte
@@ -14,7 +14,7 @@ tags = params.has("tag") ? params.getAll("tag") : [];
 categories = params.has("category") ? params.getAll("category") : [];
 const uncategorized = params.get("uncategorized");
 
-let allowedCategories = ["Quantum Chemistry", "Physical Chemistry", "Computational Chemistry", "Quantum Computing"];
+let allowedCategories = ["量子化學", "物理化學", "計算化學", "量子計算"];
 
 interface Post {
 	slug: string;
